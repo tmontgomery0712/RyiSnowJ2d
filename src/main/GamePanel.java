d4f96baba83aff2f2aa38e5166d57e490f648cd0
Trevor Montgomery <trevormontgomery13@gmail.com>
2026-08-07 14:34:45 -0400

    Add files via upload

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -17,10 +17,16 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenWidth = tileSize * maxScreenCol; //768 pixels
     public final int screenHeight = tileSize * maxScreenRow; //576 pixels
 
+    // WORLD SETTINGS
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
     TileManager tileManager = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this, this.keyH);
+    public Player player = new Player(this, this.keyH);
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
